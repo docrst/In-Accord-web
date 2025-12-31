cd887071a141958517b752e07c90ede05de7025e
Doc Cowles <docrst@gmail.com>
2025-12-30 19:17:18 -0700

    working at Dashboard page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import Dashboard from "@/app/dashboad/page";
 
 export default function Home() {
-  return (
-      <main className="flex min-h-screen flex-col items-center justify-between p24"> 
-      hello
-      </main>
-  );
+  return <Dashboard />;
 }
