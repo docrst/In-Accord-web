a77d625133b784650a4983503846364199ac66f5
Doc Cowles <docrst@gmail.com>
2025-12-31 10:43:59 -0700

    Add Redux API slice and global state management with initial reducers

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -9,7 +9,6 @@ import {
 import globalReducer from "@/state";
 import { api } from "@/state/api";
 import { setupListeners } from "@reduxjs/toolkit/query";
-
 import {
   persistStore,
   persistReducer,
