6c97f7a67c0c73e5b213dce589f0892578f342cf
Doc Cowles <docrst@gmail.com>
2026-01-01 21:52:10 -0700

    Add initial Plugins and Themes components with under construction message

---
+++ client/src/app/plugins/plugins.tsx
@@ -0,0 +1,9 @@
+const Plugins = (props: Props) => {
+    return (
+      <div>Plugins
+        Under Construction
+      </div>
+    )
+}
+
+export default Plugins
---
+++ client/src/app/themes/themes.tsx
@@ -0,0 +1,9 @@
+const Themes = (props: Props) => {
+    return (
+      <div>Themes
+        Under Construction
+      </div>
+    )
+}
+
+export default Themes
