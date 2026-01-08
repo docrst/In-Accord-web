ac6456bf52d3140782f0eb58274ead5e1bb63e7d
Doc Cowles <docrst@gmail.com>
2026-01-07 18:06:16 -0700

    feat: update Dashboard to include CardPopularProducts component for displaying popular products

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -5,12 +5,35 @@ import React from "react"
 const CardPopularProducts  = () => {
   const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
 
+  return (
+  <div className="row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl pb-16">
+    {isLoading? (
+        <div className="m-5">Loading...</div>
+     ) : (
+       <>
+         <h3 className="text-lg font-semibold px-7 pt-5 pb-2">
+            Popular Products
+         </h3>
+         <hr/>
+         <div className="overflow-auto h-full">
+           {dashboardMetrics?.popularProducts.map((product) => (
+             <div
+               key={product.productId}
+               className="flex items-center justify-between gap-3 px-5 py-7 border-b"
+             >
+            <div> img </div>
+            <div className="flex flex-col justify-between gap-1">
 
-  return <div className="row-span-3 xl:row-span-6 bg-gray-500">
+          
 
-    CardPopularProducts
-
-</div>
+            </div>   
+          </div>
+         ))}
+        </div>
+      </>
+     )}
+   </div>
+  );
 };
 
-export default CardPopularProducts;
+export default CardPopularProducts; 
--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,12 +1,14 @@
  "use client";
+
+import CardPopularProducts from "./CardPopularProducts";
+
  
  const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 p-4 custom-grid-rows">
 
-     <div className="row-span-3 xl:row-span-6 bg-gray-500" 
-     
-     />
+     <CardPopularProducts />
+    
 
      <div className="row-span-3 xl:row-span-6 bg-gray-500" 
      
