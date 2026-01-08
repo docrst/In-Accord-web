76f43353c25c955d42224bd8d3c543aaa3f6418e
Doc Cowles <docrst@gmail.com>
2026-01-07 20:21:57 -0700

    fix: correct formatting of CardPopularProducts component in Dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -6,9 +6,7 @@ import CardPopularProducts from "./CardPopularProducts";
  const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 p-4 custom-grid-rows">
-
-     <CardPopularProducts />
-    
+      <CardPopularProducts />
 
      <div className="row-span-3 xl:row-span-6 bg-gray-500" 
      
